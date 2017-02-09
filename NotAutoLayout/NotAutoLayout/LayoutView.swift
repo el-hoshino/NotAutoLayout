83a7fb5c1caeca231fcebb48ea8c2ac45db5bf03
星野恵瑠 <shonetesmith@gmail.com>
2017-02-09 23:26:20 +0900

    LayoutView declaration

---
+++ NotAutoLayout/NotAutoLayout/LayoutView.swift
@@ -0,0 +1,15 @@
+//
+//  LayoutView.swift
+//  NotAutoLayout
+//
+//  Created by 史翔新 on 2017/02/09.
+//  Copyright © 2017年 史翔新. All rights reserved.
+//
+
+import UIKit
+
+public class LayoutView: UIView {
+	
+	
+	
+}
