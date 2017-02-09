4782f5dd40b033a0183070d0264d3989b45ae27a
星野恵瑠 <shonetesmith@gmail.com>
2017-02-09 23:23:07 +0900

    Initial Xcode project

---
+++ NotAutoLayout/NotAutoLayout/NotAutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  NotAutoLayout.h
+//  NotAutoLayout
+//
+//  Created by 史翔新 on 2017/02/09.
+//  Copyright © 2017年 史翔新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotAutoLayout.
+FOUNDATION_EXPORT double NotAutoLayoutVersionNumber;
+
+//! Project version string for NotAutoLayout.
+FOUNDATION_EXPORT const unsigned char NotAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotAutoLayout/PublicHeader.h>
+
+
---
+++ NotAutoLayout/NotAutoLayoutTests/NotAutoLayoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  NotAutoLayoutTests.swift
+//  NotAutoLayoutTests
+//
+//  Created by 史翔新 on 2017/02/09.
+//  Copyright © 2017年 史翔新. All rights reserved.
+//
+
+import XCTest
+@testable import NotAutoLayout
+
+class NotAutoLayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
