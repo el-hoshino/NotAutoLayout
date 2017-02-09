021bdb27dd653469849d77757646032101a8a989
星野恵瑠 <shonetesmith@gmail.com>
2017-02-09 23:30:04 +0900

    LayoutView.RectType declaration

---
+++ NotAutoLayout/NotAutoLayout/LayoutViewRectType.swift
@@ -0,0 +1,18 @@
+//
+//  LayoutViewRectType.swift
+//  NotAutoLayout
+//
+//  Created by 史翔新 on 2017/02/09.
+//  Copyright © 2017年 史翔新. All rights reserved.
+//
+
+import Foundation
+
+extension LayoutView {
+	
+	enum RectType {
+		case absolute(CGRect)
+		case relative(CGRect)
+	}
+	
+}
