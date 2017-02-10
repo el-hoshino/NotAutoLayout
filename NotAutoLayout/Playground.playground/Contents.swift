386c2fc54fc20274a65663646dc30077139a71d1
史 翔新 <shi_xiangxin@mages.co.jp>
2017-02-10 19:11:02 +0900

    Playground content update, which is easier to read

--- NotAutoLayout/Playground.playground/Contents.swift
+++ NotAutoLayout/Playground.playground/Contents.swift
@@ -6,55 +6,74 @@ import NotAutoLayout
 
 var str = "Hello, playground"
 
-let view = LayoutView(frame: CGRect(x: 0, y: 0, width: 960, height: 640))
+let view = LayoutView(frame: CGRect(x: 0, y: 0, width: 320, height: 568))
 view.backgroundColor = .white
 PlaygroundPage.current.liveView = view
 
-let isSmallSize: (CGSize) -> Bool = { $0.width < 540 || $0.height < 640 }
+typealias LayoutOutput = (condition: LayoutView.Condition, viewA: LayoutView.Position, viewB: LayoutView.Position, viewC: LayoutView.Position)
+
+let oneThird: CGFloat = 1 / 3
+
+let landscapeLayout: LayoutOutput = {
+	let isLandscape: LayoutView.Condition = { $0.width > $0.height }
+	let viewA = LayoutView.Position.relative(CGRect(x: 0, y: 0, width: oneThird, height: 1))
+	let viewB = LayoutView.Position.relative(CGRect(x: oneThird, y: 0, width: oneThird, height: 1))
+	let viewC = LayoutView.Position.relative(CGRect(x: oneThird * 2, y: 0, width: oneThird, height: 1))
+	return (isLandscape, viewA, viewB, viewC)
+}()
+
+let otherLayout: LayoutOutput = {
+	let condition: LayoutView.Condition = { _ in true }
+	let viewA = LayoutView.Position.relative(CGRect(x: 0, y: 0, width: 1, height: oneThird))
+	let viewB = LayoutView.Position.relative(CGRect(x: 0, y: oneThird, width: 1, height: oneThird))
+	let viewC = LayoutView.Position.relative(CGRect(x: 0, y: oneThird * 2, width: 1, height: oneThird))
+	return (condition, viewA, viewB, viewC)
+}()
 
 let viewA = UIView()
 viewA.backgroundColor = .red
 
-let viewACondition1: LayoutView.Condition = isSmallSize
-let viewAPosition1 = LayoutView.Position.relative(CGRect(x: 0, y: 0, width: 0.5, height: 0.7))
-let viewALayoutMethod1: LayoutView.LayoutMethod = (viewACondition1, viewAPosition1)
-
-let viewACondition2: LayoutView.Condition = { _ in true }
-let viewAPosition2 = LayoutView.Position.absolute(CGRect(x: 0, y: 0, width: 270, height: 448))
-let viewALayoutMethod2: LayoutView.LayoutMethod = (viewACondition2, viewAPosition2)
-
-let viewATuple: LayoutView.SubviewTuple = (viewA, [viewALayoutMethod1, viewALayoutMethod2])
-
-
 let viewB = UIView()
 viewB.backgroundColor = .green
-
-let viewBCondition1: LayoutView.Condition = isSmallSize
-let viewBPosition1 = LayoutView.Position.relative(CGRect(x: 0.5, y: 0, width: 0.5, height: 0.7))
-let viewBLayoutMethod1: LayoutView.LayoutMethod = (viewBCondition1, viewBPosition1)
-
-let viewBCondition2: LayoutView.Condition = { _ in true }
-let viewBPosition2 = LayoutView.Position.absolute(CGRect(x: 270, y: 0, width: 270, height: 448))
-let viewBLayoutMethod2: LayoutView.LayoutMethod = (viewBCondition2, viewBPosition2)
-
-let viewBTuple: LayoutView.SubviewTuple = (viewB, [viewBLayoutMethod1, viewBLayoutMethod2])
-
 
 let viewC = UIView()
 viewC.backgroundColor = .blue
 
-let viewCCondition1: LayoutView.Condition = isSmallSize
-let viewCPosition1 = LayoutView.Position.relative(CGRect(x: 0, y: 0.7, width: 1, height: 0.3))
-let viewCLayoutMethod1: LayoutView.LayoutMethod = (viewCCondition1, viewCPosition1)
+let subviewTuples: [LayoutView.SubviewTuple] = {
+	
+	var viewALayoutMethods: [LayoutView.LayoutMethod] = []
+	var viewBLayoutMethods: [LayoutView.LayoutMethod] = []
+	var viewCLayoutMethods: [LayoutView.LayoutMethod] = []
+	
+	do {
+		let layout = landscapeLayout
+		let viewALayout: LayoutView.LayoutMethod = (layout.condition, layout.viewA)
+		let viewBLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewB)
+		let viewCLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewC)
+		viewALayoutMethods.append(viewALayout)
+		viewBLayoutMethods.append(viewBLayout)
+		viewCLayoutMethods.append(viewCLayout)
+	}
+	
+	do {
+		let layout = otherLayout
+		let viewALayout: LayoutView.LayoutMethod = (layout.condition, layout.viewA)
+		let viewBLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewB)
+		let viewCLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewC)
+		viewALayoutMethods.append(viewALayout)
+		viewBLayoutMethods.append(viewBLayout)
+		viewCLayoutMethods.append(viewCLayout)
+	}
+	
+	let a: LayoutView.SubviewTuple = (viewA, viewALayoutMethods)
+	let b: LayoutView.SubviewTuple = (viewB, viewBLayoutMethods)
+	let c: LayoutView.SubviewTuple = (viewC, viewCLayoutMethods)
+	
+	return [a, b, c]
 
-let viewCCondition2: LayoutView.Condition = { _ in true }
-let viewCPosition2 = LayoutView.Position.custom { CGRect(x: (540 - (min($0.width, $0.height) * 0.3)) / 2, y: $0.height * 0.7, width: min($0.width, $0.height) * 0.3, height: min($0.width, $0.height) * 0.3) }
-let viewCLayoutMethod2: LayoutView.LayoutMethod = (viewCCondition2, viewCPosition2)
+}()
 
-let viewCTuple: LayoutView.SubviewTuple = (viewC, [viewCLayoutMethod1, viewCLayoutMethod2])
-
-
-view.layoutingSubviews = [viewATuple, viewBTuple, viewCTuple]
+view.layoutingSubviews = subviewTuples
 
 view.reloadLayoutingSubviews()
 view.setNeedsLayout()
