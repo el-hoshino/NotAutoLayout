c3da002d336b5f13074188d1da988153fda28fda
史 翔新 <shi_xiangxin@mages.co.jp>
2017-02-22 21:48:24 +0900

    Playground sample update

--- NotAutoLayout/Playground.playground/Contents.swift
+++ NotAutoLayout/Playground.playground/Contents.swift
@@ -4,70 +4,77 @@ import UIKit
 import PlaygroundSupport
 import NotAutoLayout
 
-var str = "Hello, playground"
-
 let view = LayoutView(frame: CGRect(x: 0, y: 0, width: 320, height: 568))
 view.backgroundColor = .white
 PlaygroundPage.current.liveView = view
 
-typealias LayoutOutput = (condition: LayoutView.Condition, viewA: LayoutView.Position, viewB: LayoutView.Position, viewC: LayoutView.Position)
+typealias LayoutOutput = (condition: LayoutCondition, viewA: LayoutPosition, viewB: LayoutPosition, viewC: LayoutPosition)
 
 let oneThird: CGFloat = 1 / 3
 
 let landscapeLayout: LayoutOutput = {
-	let isLandscape: LayoutView.Condition = { $0.width > $0.height }
-	let viewA = LayoutView.Position.relative(CGRect(x: 0, y: 0, width: oneThird, height: 1))
-	let viewB = LayoutView.Position.relative(CGRect(x: oneThird, y: 0, width: oneThird, height: 1))
-	let viewC = LayoutView.Position.relative(CGRect(x: oneThird * 2, y: 0, width: oneThird, height: 1))
+	let isLandscape: LayoutCondition = { $0.width > $0.height }
+	let viewA = LayoutPosition.relative(CGRect(x: 0, y: 0, width: oneThird, height: 1))
+	let viewB = LayoutPosition.relative(CGRect(x: oneThird, y: 0, width: oneThird, height: 1))
+	let viewC = LayoutPosition.relative(CGRect(x: oneThird * 2, y: 0, width: oneThird, height: 1))
 	return (isLandscape, viewA, viewB, viewC)
 }()
 
 let otherLayout: LayoutOutput = {
-	let condition: LayoutView.Condition = { _ in true }
-	let viewA = LayoutView.Position.relative(CGRect(x: 0, y: 0, width: 1, height: oneThird))
-	let viewB = LayoutView.Position.relative(CGRect(x: 0, y: oneThird, width: 1, height: oneThird))
-	let viewC = LayoutView.Position.relative(CGRect(x: 0, y: oneThird * 2, width: 1, height: oneThird))
+	let condition: LayoutCondition = { _ in true }
+	let viewA = LayoutPosition.relative(CGRect(x: 0, y: 0, width: 1, height: oneThird))
+	let viewB = LayoutPosition.relative(CGRect(x: 0, y: oneThird, width: 1, height: oneThird))
+	let viewC = LayoutPosition.relative(CGRect(x: 0, y: oneThird * 2, width: 1, height: oneThird))
 	return (condition, viewA, viewB, viewC)
 }()
 
-let viewA = UIView()
-viewA.backgroundColor = .red
+let labelA: UILabel = {
+	let label = UILabel()
+	label.backgroundColor = .red
+	label.textAlignment = .center
+	label.text = "Label A"
+	return label
+}()
 
-let viewB = UIView()
-viewB.backgroundColor = .green
+let labelB: UILabel = {
+	let label = UILabel()
+	label.backgroundColor = .green
+	label.textAlignment = .center
+	label.text = "Label B"
+	return label
+}()
 
-let viewC = UIView()
-viewC.backgroundColor = .blue
+let labelC: UILabel = {
+	let label = UILabel()
+	label.backgroundColor = .blue
+	label.textAlignment = .center
+	label.text = "Label C"
+	return label
+}()
 
-let subviewTuples: [LayoutView.SubviewTuple] = {
+let subviewTuples: [SubviewTuple] = {
 	
-	var viewALayoutMethods: [LayoutView.LayoutMethod] = []
-	var viewBLayoutMethods: [LayoutView.LayoutMethod] = []
-	var viewCLayoutMethods: [LayoutView.LayoutMethod] = []
+	var viewALayoutMethods: [LayoutMethod] = []
+	var viewBLayoutMethods: [LayoutMethod] = []
+	var viewCLayoutMethods: [LayoutMethod] = []
 	
 	do {
 		let layout = landscapeLayout
-		let viewALayout: LayoutView.LayoutMethod = (layout.condition, layout.viewA)
-		let viewBLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewB)
-		let viewCLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewC)
-		viewALayoutMethods.append(viewALayout)
-		viewBLayoutMethods.append(viewBLayout)
-		viewCLayoutMethods.append(viewCLayout)
+		viewALayoutMethods.append((layout.condition, layout.viewA))
+		viewBLayoutMethods.append((layout.condition, layout.viewB))
+		viewCLayoutMethods.append((layout.condition, layout.viewC))
 	}
 	
 	do {
 		let layout = otherLayout
-		let viewALayout: LayoutView.LayoutMethod = (layout.condition, layout.viewA)
-		let viewBLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewB)
-		let viewCLayout: LayoutView.LayoutMethod = (layout.condition, layout.viewC)
-		viewALayoutMethods.append(viewALayout)
-		viewBLayoutMethods.append(viewBLayout)
-		viewCLayoutMethods.append(viewCLayout)
+		viewALayoutMethods.append((layout.condition, layout.viewA))
+		viewBLayoutMethods.append((layout.condition, layout.viewB))
+		viewCLayoutMethods.append((layout.condition, layout.viewC))
 	}
 	
-	let a: LayoutView.SubviewTuple = (viewA, viewALayoutMethods)
-	let b: LayoutView.SubviewTuple = (viewB, viewBLayoutMethods)
-	let c: LayoutView.SubviewTuple = (viewC, viewCLayoutMethods)
+	let a: SubviewTuple = (labelA, viewALayoutMethods)
+	let b: SubviewTuple = (labelB, viewBLayoutMethods)
+	let c: SubviewTuple = (labelC, viewCLayoutMethods)
 	
 	return [a, b, c]
 
