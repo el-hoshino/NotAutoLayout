a41672cba9da3ff68706171c780724216ed96efc
星野恵瑠 <shonetesmith@gmail.com>
2017-02-23 23:58:30 +0900

    LayoutPosition.offset minor update

--- NotAutoLayout/NotAutoLayout/LayoutPosition.swift
+++ NotAutoLayout/NotAutoLayout/LayoutPosition.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public enum LayoutPosition {
 	
-	public enum OffsetType {
+	public enum OffsetOrigin {
 		
 		case topLeft
 		case topCenter
@@ -31,7 +31,7 @@ public enum LayoutPosition {
 	
 	case insets(UIEdgeInsets)
 	
-	case offset(value: UIOffset, type: OffsetType, size: CGSize)
+	case offset(value: UIOffset, from: OffsetOrigin, size: CGSize)
 	
 	case customByFrame(frame: (CGSize) -> CGRect)
 	case customByOriginSize(origin: (CGSize) -> CGPoint, size: (CGSize) -> CGSize)
@@ -55,8 +55,8 @@ extension LayoutPosition {
 		case .insets(let insets):
 			return size.absolutePosition(withinInsets: insets)
 			
-		case .offset(value: let value, type: let type, size: let objectSize):
-			return size.absolutePosition(offsetBy: value, from: type, forObjectSize: objectSize)
+		case .offset(value: let value, from: let offsetOrigin, size: let objectSize):
+			return size.absolutePosition(offsetBy: value, from: offsetOrigin, forObjectSize: objectSize)
 			
 		case .customByFrame(frame: let frameTransform):
 			return size.absolutePositionAppliedTo(frame: frameTransform)
@@ -70,70 +70,6 @@ extension LayoutPosition {
 		case .custom(let transform):
 			return size.absolutePositionAppliedTo(frame: transform)
 		}
-	}
-	
-}
-
-extension LayoutPosition.OffsetType {
-	
-	func getPosition(offset: UIOffset, boundSize: CGSize, objectSize: CGSize) -> PositionRect {
-		
-		let boundHorizontalCenter = boundSize.width / 2
-		let boundVerticalCenter = boundSize.height / 2
-		
-		let objectHorizontalCenter = objectSize.width / 2
-		let objectVerticalCenter = objectSize.height / 2
-		
-		let horizontalCenterDiff = boundHorizontalCenter - objectHorizontalCenter
-		let verticalCenterDiff = boundVerticalCenter - objectVerticalCenter
-		
-		let baseX: CGFloat
-		let baseY: CGFloat
-		
-		switch self {
-		case .topLeft:
-			baseX = offset.horizontal - horizontalCenterDiff
-			baseY = offset.vertical - verticalCenterDiff
-			
-		case .topCenter:
-			baseX = offset.horizontal
-			baseY = offset.vertical - verticalCenterDiff
-			
-		case .topRight:
-			baseX = offset.horizontal + horizontalCenterDiff
-			baseY = offset.vertical - verticalCenterDiff
-			
-		case .middleLeft:
-			baseX = offset.horizontal - horizontalCenterDiff
-			baseY = offset.vertical
-			
-		case .middleCenter:
-			baseX = offset.horizontal
-			baseY = offset.vertical
-			
-		case .middleRight:
-			baseX = offset.horizontal + horizontalCenterDiff
-			baseY = offset.vertical
-			
-		case .bottomLeft:
-			baseX = offset.horizontal - horizontalCenterDiff
-			baseY = offset.vertical + verticalCenterDiff
-			
-		case .bottomCenter:
-			baseX = offset.horizontal
-			baseY = offset.vertical + verticalCenterDiff
-			
-		case .bottomRight:
-			baseX = offset.horizontal + horizontalCenterDiff
-			baseY = offset.vertical + verticalCenterDiff
-		}
-		
-		let centerX = baseX + boundHorizontalCenter
-		let centerY = baseY + boundVerticalCenter
-		let center = CGPoint(x: centerX, y: centerY)
-		
-		return PositionRect(center: center, size: objectSize)
-		
 	}
 	
 }
