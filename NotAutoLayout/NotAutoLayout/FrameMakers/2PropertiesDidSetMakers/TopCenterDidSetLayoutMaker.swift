77c4eb46c53a117b5ce68104a15debf1cc339341
史 翔新 <shi_xiangxin@mages.co.jp>
2017-07-07 15:20:02 +0900

    Add some layout making methods

--- NotAutoLayout/NotAutoLayout/FrameMakers/2PropertiesDidSetMakers/TopCenterDidSetLayoutMaker.swift
+++ NotAutoLayout/NotAutoLayout/FrameMakers/2PropertiesDidSetMakers/TopCenterDidSetLayoutMaker.swift
@@ -18,6 +18,46 @@ public struct TopCenterDidSetLayoutMaker {
 
 extension TopCenterDidSetLayoutMaker {
 	
+	public func setBottom(to bottom: CGFloat) -> TopCenterBottomDidSetLayoutMaker {
+		
+		let bottom = CGRect.Float.constant(bottom)
+		
+		let maker = TopCenterBottomDidSetLayoutMaker(parentView: self.parentView,
+		                                             topCenter: self.topCenter,
+		                                             bottom: bottom)
+		
+		return maker
+		
+	}
+	
+	public func pinBottom(to referenceView: UIView, s reference: CGRect.VerticalBasePoint, offsetBy offset: CGFloat = 0, ignoresTransform: Bool = false) -> TopCenterBottomDidSetLayoutMaker {
+		
+		let bottom = self.parentView.verticalReference(reference, of: referenceView, offsetBy: offset, ignoresTransform: ignoresTransform)
+		
+		let maker = TopCenterBottomDidSetLayoutMaker(parentView: self.parentView,
+		                                             topCenter: self.topCenter,
+		                                             bottom: bottom)
+		
+		return maker
+		
+	}
+	
+	public func calculateBottom(by calculation: @escaping (_ boundSize: CGSize) -> CGFloat) -> TopCenterBottomDidSetLayoutMaker {
+		
+		let bottom = CGRect.Float.closure(calculation)
+		
+		let maker = TopCenterBottomDidSetLayoutMaker(parentView: self.parentView,
+		                                             topCenter: self.topCenter,
+		                                             bottom: bottom)
+		
+		return maker
+		
+	}
+	
+}
+
+extension TopCenterDidSetLayoutMaker {
+	
 	public func setWidth(to width: CGFloat) -> TopCenterWidthDidSetLayoutMaker {
 		
 		let width = CGRect.Float.constant(width)
--- NotAutoLayout/NotAutoLayout/FrameMakers/3PropertiesDidSetMakers/TopCenterBottomDidSetLayoutMaker.swift
+++ NotAutoLayout/NotAutoLayout/FrameMakers/3PropertiesDidSetMakers/TopCenterBottomDidSetLayoutMaker.swift
@@ -17,3 +17,62 @@ public struct TopCenterBottomDidSetLayoutMaker {
 	let bottom: CGRect.Float
 	
 }
+
+extension TopCenterBottomDidSetLayoutMaker {
+	
+	private func makeFrame(topCenter: CGPoint, bottom: CGFloat, width: CGFloat) -> CGRect {
+		
+		let height = bottom - topCenter.y
+		let x = topCenter.x - width.half
+		let y = topCenter.y
+		let frame = CGRect(x: x, y: y, width: width, height: height)
+		
+		return frame
+		
+	}
+	
+}
+
+extension TopCenterBottomDidSetLayoutMaker {
+	
+	public func setWidth(to width: CGFloat) -> Layout.Individual {
+		
+		if let topCenter = self.topCenter.constantValue, let bottom = self.bottom.constantValue {
+			let frame = self.makeFrame(topCenter: topCenter, bottom: bottom, width: width)
+			let layout = Layout.Individual.makeAbsolute(from: frame)
+			
+			return layout
+			
+		} else {
+			let layout = Layout.Individual.makeCustom { (boundSize) -> CGRect in
+				let topCenter = self.topCenter.closureValue(boundSize)
+				let bottom = self.bottom.closureValue(boundSize)
+				let frame = self.makeFrame(topCenter: topCenter, bottom: bottom, width: width)
+				
+				return frame
+				
+			}
+			
+			return layout
+			
+		}
+		
+	}
+	
+	public func calculateWidth(by calculation: @escaping (_ boundSize: CGSize) -> CGFloat) -> Layout.Individual {
+		
+		let layout = Layout.Individual.makeCustom { (boundSize) -> CGRect in
+			let topCenter = self.topCenter.closureValue(boundSize)
+			let bottom = self.bottom.closureValue(boundSize)
+			let width = calculation(boundSize)
+			let frame = self.makeFrame(topCenter: topCenter, bottom: bottom, width: width)
+			
+			return frame
+			
+		}
+		
+		return layout
+		
+	}
+	
+}
