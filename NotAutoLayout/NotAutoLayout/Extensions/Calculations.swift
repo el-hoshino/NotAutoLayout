144d9aacfe3370b0763711e9f57e7bfa3dd632ec
史 翔新 <shi_xiangxin@mages.co.jp>
2017-07-14 17:37:09 +0900

    Some new calculation methods for internal use

--- NotAutoLayout/NotAutoLayout/Extensions/Calculations.swift
+++ NotAutoLayout/NotAutoLayout/Extensions/Calculations.swift
@@ -15,9 +15,73 @@ func + (lhs: CGPoint, rhs: CGVector) -> CGPoint {
 	
 }
 
+func += (lhs: inout CGPoint, rhs: CGVector) {
+	
+	lhs = lhs + rhs
+	
+}
+
 func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
 	
 	let result = CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
 	return result
+	
+}
+
+func += (lhs: inout CGPoint, rhs: CGPoint) {
+	
+	lhs = lhs + rhs
+	
+}
+
+func + (lhs: CGSize, rhs: CGSize) -> CGSize {
+	
+	let result = CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+	return result
+	
+}
+
+func += (lhs: inout CGSize, rhs: CGSize) {
+	
+	lhs = lhs + rhs
+	
+}
+
+func - (lhs: CGPoint, rhs: CGVector) -> CGPoint {
+	
+	let result = CGPoint(x: lhs.x - rhs.dx, y: lhs.y - rhs.dy)
+	return result
+	
+}
+
+func -= (lhs: inout CGPoint, rhs: CGVector) {
+	
+	lhs = lhs - rhs
+	
+}
+
+func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+	
+	let result = CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+	return result
+	
+}
+
+func -= (lhs: inout CGPoint, rhs: CGPoint) {
+	
+	lhs = lhs - rhs
+	
+}
+
+func - (lhs: CGSize, rhs: CGSize) -> CGSize {
+	
+	let result = CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+	return result
+	
+}
+
+func -= (lhs: inout CGSize, rhs: CGSize) {
+	
+	lhs = lhs - rhs
 	
 }
