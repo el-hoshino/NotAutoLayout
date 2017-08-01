b6989cccf626a2cc0f662657d92d1ba37f371471
史 翔新 <shi_xiangxin@mages.co.jp>
2017-08-01 12:45:31 +0900

    LayoutView and LayoutScrollView declaration fix

--- NotAutoLayout/NotAutoLayout/LayoutEngine/LayoutScrollView.swift
+++ NotAutoLayout/NotAutoLayout/LayoutEngine/LayoutScrollView.swift
@@ -10,15 +10,23 @@ import Foundation
 
 open class LayoutScrollView: UIScrollView, LayoutControllable {
 	
-	public var layoutInfo: [ConditionEnum.RawValue: LayoutInfo] = [:]
-	public var orderInfo: [ConditionEnum.RawValue : OrderInfo] = [:]
-	public var zIndexInfo: [ConditionEnum.RawValue : ZIndexInfo] = [:]
+	open var layoutInfo: [ConditionEnum.RawValue: LayoutInfo] = [:]
+	open var orderInfo: [ConditionEnum.RawValue : OrderInfo] = [:]
+	open var zIndexInfo: [ConditionEnum.RawValue : ZIndexInfo] = [:]
 	
-	public var layoutOptimization: LayoutOptimization = .none
+	open var layoutOptimization: LayoutOptimization = .none
+	
+	open func getDefaultCondition() -> ConditionEnum {
+		return DefaultCondition.default
+	}
+	
+	open func getCondition(underCurrentBoundSize boundSize: CGSize) -> ConditionEnum? {
+		return nil
+	}
 	
 	open override func layoutSubviews() {
 		super.layoutSubviews()
 		self.nal.layoutSubviews()
 	}
-	
+
 }
--- NotAutoLayout/NotAutoLayout/LayoutEngine/LayoutView.swift
+++ NotAutoLayout/NotAutoLayout/LayoutEngine/LayoutView.swift
@@ -11,11 +11,19 @@ import UIKit
 /// The very basic `UIView` that conforms `LayoutControllable` protocol, to layout subviews.
 open class LayoutView: UIView, LayoutControllable {
 	
-	public var layoutInfo: [ConditionEnum.RawValue: LayoutInfo] = [:]
-	public var orderInfo: [ConditionEnum.RawValue : OrderInfo] = [:]
-	public var zIndexInfo: [ConditionEnum.RawValue : ZIndexInfo] = [:]
+	open var layoutInfo: [ConditionEnum.RawValue: LayoutInfo] = [:]
+	open var orderInfo: [ConditionEnum.RawValue : OrderInfo] = [:]
+	open var zIndexInfo: [ConditionEnum.RawValue : ZIndexInfo] = [:]
 	
-	public var layoutOptimization: LayoutOptimization = .none
+	open var layoutOptimization: LayoutOptimization = .none
+	
+	open func getDefaultCondition() -> ConditionEnum {
+		return DefaultCondition.default
+	}
+	
+	open func getCondition(underCurrentBoundSize boundSize: CGSize) -> ConditionEnum? {
+		return nil
+	}
 	
 	open override func layoutSubviews() {
 		super.layoutSubviews()
