7d0e142fb4c3023fafd983c3826c96475a76ce7b
史 翔新 <shi_xiangxin@mages.co.jp>
2017-09-15 14:35:08 +0900

    Update playground

--- NotAutoLayout/Playground.playground/Contents.swift
+++ NotAutoLayout/Playground.playground/Contents.swift
@@ -1,171 +1,29 @@
 //: Playground - noun: a place where people can play
-
-import UIKit
 import PlaygroundSupport
 import NotAutoLayout
 
-let baseView = LayoutInfoStoredView(frame: CGRect(x: 0, y: 0, width: 320, height: 568))
-PlaygroundPage.current.liveView = baseView
-
-baseView.backgroundColor = .white
-
-public class ProfileSummaryView: LayoutInfoStoredView {
-	
-	private let avatarView: UIImageView
-	private let mainTitleLabel: UILabel
-	private let subTitleLabel: UILabel
-	
-	private let avatarSize: CGSize = .init(width: 50, height: 50)
-	private let margin: CGFloat = 10
-	
-	public var avatar: UIImage? {
-		get {
-			return self.avatarView.image
-		}
-		set {
-			self.avatarView.image = newValue
-		}
-	}
-	
-	public var mainTitle: String? {
-		get {
-			return self.mainTitleLabel.text
-		}
-		set {
-			self.mainTitleLabel.text = newValue
-		}
-	}
-	
-	public var subTitle: String? {
-		get {
-			return self.subTitleLabel.text
-		}
-		set {
-			self.subTitleLabel.text = newValue
-		}
-	}
-	
-	public override init(frame: CGRect) {
-		
-		self.avatarView = UIImageView()
-		self.mainTitleLabel = UILabel()
-		self.subTitleLabel = UILabel()
-		
-		super.init(frame: frame)
-		
-		self.setupAvatarView()
-		self.setupMainTitleView()
-		self.setupSubTitleView()
-		
-	}
-	
-	public convenience init() {
-		self.init(frame: .zero)
-	}
-	
-	public required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-	
-	public override func sizeThatFits(_ size: CGSize) -> CGSize {
-		
-		let widthExcludeLabel = self.avatarSize.width + self.margin
-		let labelWidth = size.width - widthExcludeLabel
-		let labelFittingSize = CGSize(width: labelWidth, height: 0)
-		let mainFittedSize = self.mainTitleLabel.sizeThatFits(labelFittingSize)
-		let subFittedSize = self.subTitleLabel.sizeThatFits(labelFittingSize)
-		let labelMaxWidth = max(mainFittedSize.width, subFittedSize.width)
-		let fittedWidth = labelMaxWidth + widthExcludeLabel
-		let fittedHeight = self.avatarSize.height
-		
-		return CGSize(width: fittedWidth, height: fittedHeight)
-		
-	}
-	
-	public override func layoutSubviews() {
-		super.layoutSubviews()
-		self.placeAvatarView()
-		self.placeMainTitleView()
-		self.placeSubTitleView()
-	}
-	
-}
-
-extension ProfileSummaryView {
-	
-	private func setupAvatarView() {
-		
-		self.avatarView.backgroundColor = .red
-		self.avatarView.clipsToBounds = true
-		self.addSubview(self.avatarView)
-		
-	}
-	
-	private func setupMainTitleView() {
-		
-		self.mainTitleLabel.clipsToBounds = true
-		self.mainTitleLabel.font = .boldSystemFont(ofSize: 14)
-		self.mainTitleLabel.textColor = .black
-		self.addSubview(self.mainTitleLabel)
-		
-	}
-	
-	private func setupSubTitleView() {
-		
-		self.subTitleLabel.clipsToBounds = true
-		self.subTitleLabel.font = .systemFont(ofSize: 14)
-		self.subTitleLabel.textColor = .darkGray
-		self.addSubview(self.subTitleLabel)
-		
-	}
-	
-}
-
-extension ProfileSummaryView {
-	
-	private func placeAvatarView() {
-		
-		self.nal.place(self.avatarView) { $0
-			.pinMiddleLeft(to: $0.parentView, s: .middleLeft)
-			.setSize(to: self.avatarSize)
-			.addingProcess(by: { (view, frame, _) in
-				view.layer.cornerRadius = min(frame.width, frame.height) / 2
-			})
-		}
-		
-	}
-	
-	private func placeMainTitleView() {
-		
-		self.nal.place(self.mainTitleLabel) { $0
-			.pinTopRight(to: $0.parentView, s: .topRight)
-			.pinLeft(to: self.avatarView, s: .right, offsetBy: self.margin)
-			.pinBottom(to: $0.parentView, s: .middle)
-		}
-		
-	}
-	
-	private func placeSubTitleView() {
-		
-		self.nal.place(self.subTitleLabel) { $0
-			.pinTopRight(to: $0.parentView, s: .middleRight)
-			.pinLeft(to: self.avatarView, s: .right, offsetBy: self.margin)
-			.pinBottom(to: $0.parentView, s: 0.75)
-		}
-		
-	}
-	
-}
+let view = LayoutInfoStoredView(frame: .init(x: 0, y: 0, width: 320, height: 568))
+view.backgroundColor = .white
+PlaygroundPage.current.liveView = view
 
 let margin: CGSize = .init(width: 10, height: 10)
-
 let summaryView = ProfileSummaryView()
+let contentsView = ContentsView()
+let replyView = ReplyView()
 
 summaryView.avatar = #imageLiteral(resourceName: "avatar.png")
 summaryView.mainTitle = "星野恵瑠＠今日も1日フレンズ㌠"
 summaryView.subTitle = "@lovee"
+contentsView.contents = """
+	Hello, NotAutoLayout!
+	The whole new NotAutoLayout 2.0 now has much better syntax which is:
+	- Easier to write (thanks to method-chain structures)
+	- Easier to read (thanks to more natural English-like statements)
+	- Even supports dynamic view sizes (with commands like `fitSize()`)!
+	"""
+contentsView.timeStamp = Date()
 
-baseView.nal.setupSubview(summaryView) { $0
+view.nal.setupSubview(summaryView) { $0
 	.makeDefaultLayout { $0
 		.pinLeft(to: $0.parentView, s: .left, offsetBy: margin.width)
 		.pinRight(to: $0.parentView, s: .right, offsetBy: -margin.width)
@@ -174,8 +32,23 @@ baseView.nal.setupSubview(summaryView) { $0
 	}
 	.addToParent()
 }
+view.nal.setupSubview(contentsView) { $0
+	.makeDefaultLayout({ $0
+		.pinTopLeft(to: summaryView, s: .bottomLeft)
+		.pinRight(to: summaryView, s: .right)
+		.fitHeight()
+		.movingY(by: margin.height)
+	})
+	.setDefaultOrder(to: 1)
+	.addToParent()
+}
+view.nal.setupSubview(replyView) { $0
+	.makeDefaultLayout({ $0
+		.pinBottomCenter(to: $0.parentView, s: .bottomCenter)
+		.setWidth(by: { $0.boundWidth - (margin.width * 2) })
+		.fitHeight()
+	})
+	.addToParent()
+}
 
-
-
-baseView.setNeedsLayout()
-
+view.setNeedsLayout()
--- NotAutoLayout/Playground.playground/Sources/ProfileSummaryView.swift
+++ NotAutoLayout/Playground.playground/Sources/ProfileSummaryView.swift
@@ -1,4 +1,153 @@
 import UIKit
 import NotAutoLayout
 
+public class ProfileSummaryView: UIView {
+	
+	private let avatarView: UIImageView
+	private let mainTitleLabel: UILabel
+	private let subTitleLabel: UILabel
+	
+	private let avatarSize: CGSize = .init(width: 50, height: 50)
+	private let margin: CGFloat = 10
+	
+	public var avatar: UIImage? {
+		get {
+			return self.avatarView.image
+		}
+		set {
+			self.avatarView.image = newValue
+		}
+	}
+	
+	public var mainTitle: String? {
+		get {
+			return self.mainTitleLabel.text
+		}
+		set {
+			self.mainTitleLabel.text = newValue
+		}
+	}
+	
+	public var subTitle: String? {
+		get {
+			return self.subTitleLabel.text
+		}
+		set {
+			self.subTitleLabel.text = newValue
+		}
+	}
+	
+	public override init(frame: CGRect) {
+		
+		self.avatarView = UIImageView()
+		self.mainTitleLabel = UILabel()
+		self.subTitleLabel = UILabel()
+		
+		super.init(frame: frame)
+		
+		self.setupAvatarView()
+		self.setupMainTitleView()
+		self.setupSubTitleView()
+		
+	}
+	
+	public convenience init() {
+		self.init(frame: .zero)
+	}
+	
+	public required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	public override func sizeThatFits(_ size: CGSize) -> CGSize {
+		
+		let widthExcludeLabel = self.avatarSize.width + self.margin
+		let labelWidth = size.width - widthExcludeLabel
+		let labelFittingSize = CGSize(width: labelWidth, height: 0)
+		let mainFittedSize = self.mainTitleLabel.sizeThatFits(labelFittingSize)
+		let subFittedSize = self.subTitleLabel.sizeThatFits(labelFittingSize)
+		let labelMaxWidth = max(mainFittedSize.width, subFittedSize.width)
+		let fittedWidth = labelMaxWidth + widthExcludeLabel
+		let fittedHeight = self.avatarSize.height
+		
+		return CGSize(width: fittedWidth, height: fittedHeight)
+		
+	}
+	
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		self.placeAvatarView()
+		self.placeMainTitleView()
+		self.placeSubTitleView()
+	}
+	
+}
 
+extension ProfileSummaryView {
+	
+	private func setupAvatarView() {
+		
+		let view = self.avatarView
+		view.backgroundColor = .red
+		view.clipsToBounds = true
+		self.addSubview(view)
+		
+	}
+	
+	private func setupMainTitleView() {
+		
+		let view = self.mainTitleLabel
+		view.clipsToBounds = true
+		view.font = .boldSystemFont(ofSize: UIFont.smallSystemFontSize)
+		view.textColor = .black
+		self.addSubview(view)
+		
+	}
+	
+	private func setupSubTitleView() {
+		
+		let view = self.subTitleLabel
+		view.clipsToBounds = true
+		view.font = .systemFont(ofSize: UIFont.smallSystemFontSize)
+		view.textColor = .darkGray
+		self.addSubview(view)
+		
+	}
+	
+}
+
+extension ProfileSummaryView {
+	
+	private func placeAvatarView() {
+		
+		self.nal.layout(self.avatarView) { $0
+			.pinMiddleLeft(to: $0.parentView, s: .middleLeft)
+			.setSize(to: self.avatarSize)
+			.addingProcess(by: { (view, frame, _) in
+				view.layer.cornerRadius = min(frame.width, frame.height) / 2
+			})
+		}
+		
+	}
+	
+	private func placeMainTitleView() {
+		
+		self.nal.layout(self.mainTitleLabel) { $0
+			.pinTopRight(to: $0.parentView, s: .topRight)
+			.pinLeft(to: self.avatarView, s: .right, offsetBy: self.margin)
+			.pinBottom(to: $0.parentView, s: .middle)
+		}
+		
+	}
+	
+	private func placeSubTitleView() {
+		
+		self.nal.layout(self.subTitleLabel) { $0
+			.pinTopRight(to: $0.parentView, s: .middleRight)
+			.pinLeft(to: self.avatarView, s: .right, offsetBy: self.margin)
+			.pinBottom(to: $0.parentView, s: 0.75)
+		}
+		
+	}
+	
+}
