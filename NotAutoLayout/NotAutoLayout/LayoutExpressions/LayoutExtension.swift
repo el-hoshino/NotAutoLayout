97fb427d4bbd9f9df8344418d28e717c0bd9c191
史 翔新 <shi_xiangxin@mages.co.jp>
2017-09-25 19:53:40 +0900

    Move LayoutInfoStorable into a specific subfolder

---
+++ NotAutoLayout/NotAutoLayout/LayoutExpressions/LayoutExtension.swift
@@ -0,0 +1,22 @@
+//
+//  Layout.swift
+//  NotAutoLayout
+//
+//  Created by 史　翔新 on 2017/02/22.
+//  Copyright © 2017年 史翔新. All rights reserved.
+//
+
+import Foundation
+
+/// An `enum` used to store the `Frame` value, or the closure(s) to retrieve the `Frame` value, from serveral ways available.
+extension Layout {
+	
+	public struct Sequential {
+		
+	}
+	
+	public struct Matrical {
+		
+	}
+	
+}
