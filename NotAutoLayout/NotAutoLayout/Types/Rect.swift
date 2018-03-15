0e569947f49af67e7ca433b5f5378e18568d17e3
史 翔新 <x_shi@yumemi.co.jp>
2018-03-15 18:51:28 +0900

    Add more properties to Rect

--- NotAutoLayout/NotAutoLayout/Types/Rect.swift
+++ NotAutoLayout/NotAutoLayout/Types/Rect.swift
@@ -35,6 +35,86 @@ extension Rect {
 }
 
 extension Rect {
+    
+    var left: CGFloat {
+        return self.origin.x
+    }
+    
+    var center: CGFloat {
+        return self.horizontalGeometry(at: 0.5)
+    }
+    
+    var right: CGFloat {
+        return self.left + self.width
+    }
+    
+    var width: CGFloat {
+        return self.size.width
+    }
+    
+}
+
+extension Rect {
+    
+    var top: CGFloat {
+        return self.origin.y
+    }
+    
+    var middle: CGFloat {
+        return self.verticalGeometry(at: 0.5)
+    }
+    
+    var bottom: CGFloat {
+        return self.top + self.height
+    }
+    
+    var height: CGFloat {
+        return self.size.height
+    }
+    
+}
+
+extension Rect {
+    
+    var topLeft: CGPoint {
+        return .init(x: self.left, y: self.top)
+    }
+    
+    var topCenter: CGPoint {
+        return .init(x: self.center, y: self.top)
+    }
+    
+    var topRight: CGPoint {
+        return .init(x: self.right, y: self.top)
+    }
+    
+    var middleLeft: CGPoint {
+        return .init(x: self.left, y: self.middle)
+    }
+    
+    var middleCenter: CGPoint {
+        return .init(x: self.center, y: self.middle)
+    }
+    
+    var middleRight: CGPoint {
+        return .init(x: self.right, y: self.middle)
+    }
+    
+    var bottomLeft: CGPoint {
+        return .init(x: self.left, y: self.bottom)
+    }
+    
+    var bottomCenter: CGPoint {
+        return .init(x: self.center, y: self.bottom)
+    }
+    
+    var bottomRight: CGPoint {
+        return .init(x: self.right, y: self.bottom)
+    }
+    
+}
+
+extension Rect {
 	
 	func horizontalGeometry(at coordinate: CGFloat) -> CGFloat {
 		return self.origin.x + (self.size.width * coordinate)
@@ -54,4 +134,16 @@ extension Rect {
 		return self.pointGeometry(x: coordinate.x, y: coordinate.y)
 	}
 	
+}
+
+extension Rect {
+    
+    var frame: CGRect {
+        return .init(origin: self.origin, size: self.size)
+    }
+    
+    func frame(inside insets: UIEdgeInsets) -> CGRect {
+        return self.frame.frame(inside: insets)
+    }
+    
 }
