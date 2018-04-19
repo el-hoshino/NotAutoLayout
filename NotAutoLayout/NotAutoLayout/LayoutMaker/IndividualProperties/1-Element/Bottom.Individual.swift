f4fe94472e86d9697b85ba1d09cbfec37c065ae8
史 翔新 <x_shi@yumemi.co.jp>
2018-04-19 16:33:52 +0900

    Define basic Top, Middle, Bottom, Width and Height properties

---
+++ NotAutoLayout/NotAutoLayout/LayoutMaker/IndividualProperties/1-Element/Bottom.Individual.swift
@@ -0,0 +1,19 @@
+//
+//  Bottom.Individual.swift
+//  NotAutoLayout
+//
+//  Created by 史 翔新 on 2018/04/19.
+//  Copyright © 2018年 史翔新. All rights reserved.
+//
+
+import Foundation
+
+extension IndividualProperty {
+	
+	public struct Bottom {
+		
+		let bottom: LayoutElement.Vertical
+		
+	}
+	
+}
---
+++ NotAutoLayout/NotAutoLayout/LayoutMaker/IndividualProperties/1-Element/Middle.Individual.swift
@@ -0,0 +1,19 @@
+//
+//  Middle.Individual.swift
+//  NotAutoLayout
+//
+//  Created by 史 翔新 on 2018/04/19.
+//  Copyright © 2018年 史翔新. All rights reserved.
+//
+
+import Foundation
+
+extension IndividualProperty {
+	
+	public struct Middle {
+		
+		let middle: LayoutElement.Vertical
+		
+	}
+	
+}
