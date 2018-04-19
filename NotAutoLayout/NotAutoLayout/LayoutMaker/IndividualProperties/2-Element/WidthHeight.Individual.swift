6c5891799e4382f40be2e7a70f561d4a98f079c3
史 翔新 <x_shi@yumemi.co.jp>
2018-04-19 16:46:23 +0900

    Add WidthHeight and Size properties

---
+++ NotAutoLayout/NotAutoLayout/LayoutMaker/IndividualProperties/2-Element/WidthHeight.Individual.swift
@@ -0,0 +1,21 @@
+//
+//  WidthHeight.Individual.swift
+//  NotAutoLayout
+//
+//  Created by 史 翔新 on 2018/04/19.
+//  Copyright © 2018年 史翔新. All rights reserved.
+//
+
+import Foundation
+
+extension IndividualProperty {
+	
+	public struct WidthHeight {
+		
+		let width: LayoutElement.Length
+		
+		let height: LayoutElement.Length
+		
+	}
+	
+}
