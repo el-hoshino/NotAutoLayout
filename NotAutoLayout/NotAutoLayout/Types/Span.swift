f34fc03d23988afacf90eb14ed31f041da4b9a58
星野恵瑠 <shonetesmith@gmail.com>
2018-05-11 23:11:53 +0900

    Add a internal common initializer for Span, so I can write a meta programming script in the future to make the LayoutProperties

--- NotAutoLayout/NotAutoLayout/Types/Span.swift
+++ NotAutoLayout/NotAutoLayout/Types/Span.swift
@@ -127,3 +127,110 @@ extension Span: CustomStringConvertible {
 	}
 	
 }
+
+// This section is for meta programming, which I'm planning in the future.
+extension Span {
+	
+	internal enum Horizontal {
+		case left(Float)
+		case center(Float)
+		case right(Float)
+		case width(Float)
+	}
+	
+	internal enum Vertical {
+		case top(Float)
+		case middle(Float)
+		case bottom(Float)
+		case height(Float)
+	}
+	
+	init(_ a: Horizontal, _ b: Horizontal) {
+		switch (a, b) {
+		case (.left(let left), .center(let center)):
+			self.init(start: left, half: center)
+			
+		case (.left(let left), .right(let right)):
+			self.init(start: left, end: right)
+			
+		case (.left(let left), .width(let width)):
+			self.init(start: left, length: width)
+			
+		case (.center(let center), .left(let left)):
+			self.init(start: left, half: center)
+			
+		case (.center(let center), .right(let right)):
+			self.init(half: center, end: right)
+			
+		case (.center(let center), .width(let width)):
+			self.init(half: center, length: width)
+			
+		case (.right(let right), .left(let left)):
+			self.init(start: left, end: right)
+			
+		case (.right(let right), .center(let center)):
+			self.init(half: center, end: right)
+			
+		case (.right(let right), .width(let width)):
+			self.init(end: right, length: width)
+			
+		case (.width(let width), .left(let left)):
+			self.init(start: left, length: width)
+			
+		case (.width(let width), .center(let center)):
+			self.init(half: center, length: width)
+			
+		case (.width(let width), .right(let right)):
+			self.init(end: right, length: width)
+			
+		case (.left, .left), (.center, .center), (.right, .right), (.width, .width):
+			assertionFailure("Failed to create a span from 2 horizontal elements. These 2 should be different from each other")
+			self = .zero
+		}
+	}
+	
+	init(_ a: Vertical, _ b: Vertical) {
+		switch (a, b) {
+		case (.top(let top), .middle(let middle)):
+			self.init(start: top, half: middle)
+			
+		case (.top(let top), .bottom(let bottom)):
+			self.init(start: top, end: bottom)
+			
+		case (.top(let top), .height(let height)):
+			self.init(start: top, length: height)
+			
+		case (.middle(let middle), .top(let top)):
+			self.init(start: top, half: middle)
+			
+		case (.middle(let middle), .bottom(let bottom)):
+			self.init(half: middle, end: bottom)
+			
+		case (.middle(let middle), .height(let height)):
+			self.init(half: middle, length: height)
+			
+		case (.bottom(let bottom), .top(let top)):
+			self.init(start: top, end: bottom)
+			
+		case (.bottom(let bottom), .middle(let middle)):
+			self.init(half: middle, end: bottom)
+			
+		case (.bottom(let bottom), .height(let height)):
+			self.init(end: bottom, length: height)
+			
+		case (.height(let height), .top(let top)):
+			self.init(start: top, length: height)
+			
+		case (.height(let height), .middle(let middle)):
+			self.init(half: middle, length: height)
+			
+		case (.height(let height), .bottom(let bottom)):
+			self.init(end: bottom, length: height)
+			
+		case (.top, .top), (.middle, .middle), (.bottom, .bottom), (.height, .height):
+			assertionFailure("Failed to create a span from 2 vertical elements. These 2 should be different from each other")
+			self = .zero
+		}
+	}
+	
+}
