293c67dfa4be6a1fa298864464d87eea6595fe48
史 翔新 <x_shi@yumemi.co.jp>
2018-05-14 18:21:07 +0900

    Add Dictionary tests

--- NotAutoLayout/NotAutoLayout/Extensions/Dictionary.swift
+++ NotAutoLayout/NotAutoLayout/Extensions/Dictionary.swift
@@ -10,36 +10,6 @@ import Foundation
 
 extension Dictionary {
 	
-	static func + (lhs: Dictionary<Key, Value>, rhs: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
-		
-		return lhs.appending(rhs)
-		
-	}
-	
-}
-
-extension Dictionary {
-	
-	func appending(_ appending: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
-		
-		var result = self
-		result.append(appending)
-		return result
-		
-	}
-	
-	mutating func append(_ appending: Dictionary<Key, Value>) {
-		
-		for pair in appending {
-			self[pair.key] = pair.value
-		}
-		
-	}
-	
-}
-
-extension Dictionary {
-	
 	func containsKey(_ key: Key) -> Bool {
 		return self[key] != nil
 	}
